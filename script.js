b4382f9ebecd88c83a8823b94b3da02b5da77d27
Amit Verma <143615149+AmitVerma1575@users.noreply.github.com>
2026-04-16 22:09:06 +0530

    completed

--- script.js
+++ script.js
@@ -30,14 +30,12 @@ const longi = position.coords.longitude
 
   const myresult = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=b04e686561e445128e5193221261304&q=${lati},${longi}&days=1&aqi=yes&alerts=no
 `)
-      const data = await myresult.json(); // ✅ REQUIRED
+      const data = await myresult.json(); 
 
 
-city.textContent = `${data.location.name}, ${data.location.country}, `
-temp.textContent = `🌡️ ${data.current.temp_c}`
+city.textContent = `${data.location.name}, ${data.location.country} `
+temp.textContent = `🌡️ ${data.current.temp_c} °C`
 condition.textContent = `🌤️${data.current.condition.text}`
-
-
   
  }
 
@@ -55,38 +53,35 @@ navigator.geolocation.getCurrentPosition(gotMyLocation,failed);
 //------------------------END--------------------------------------
 
 
+//----------------------------Show dta Search ka kunction----------------------
 
 
 
+ async function Search_data_show(place){
+
+ const searchResult = await fetch(`http://api.weatherapi.com/v1/current.json?key=6f080609e4854b90906155401261604&q=${place}&aqi=yes`) 
+
+const data_search  = await  searchResult.json();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+city.textContent = `${data_search.location.name}, ${data_search.location.country}, `
+temp.textContent = `🌡️ ${data_search.current.temp_c} °C`
+condition.textContent = `🌤️${data_search.current.condition.text}`
+  
+}
 
 
 
 //-----------------------------Search Pe EventListener---------------------
 
 search.addEventListener("click" ,(e)=>{
+  
  const place = inpt.value.trim();
+ e.preventDefault();
  if(place===""){
-  alert(" Input Was Empty Enter Something..")
+  alert(" Input Was Empty Enter Something")
  }
  else{
   Search_data_show(place);
  }
-
 })
